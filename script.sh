2549ae54da4ba6a005d6c99363dd56e5bc438778
somebody-dev <shanran_1010hh@163.com>
2025-03-02 23:35:44 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,3 +10,5 @@ rm -rf package-temp
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
 mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
 rm -rf package-temp
+
+# sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
